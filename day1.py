0114d4eb67641fcb8d0d06aa2f7175380c999d9b
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-09-20 13:53:31 +0300

    input

--- day1.py
+++ day1.py
@@ -1,6 +1,21 @@
 # print("Hello" + " Mitch")
 
-print("Day 1 - String Manipulation")  
-print('String Conctenation is done with the "+" sign.')
-print('e.g. print("hello" + "world")')
-print("New lines can be created with a bcakslash and n.")
+# print("Day 1 - String Manipulation")  
+# print('String Conctenation is done with the "+" sign.')
+# print('e.g. print("hello" + "world")')
+# print("New lines can be created with a bcakslash and n.")
+
+# Input method
+# input("what is your name?")
+# print("Hello " + input("what is your name?"))
+
+# def main(name):
+#     my_name = len(name)
+#     return my_name
+
+# name = "Mitchell"
+# print(main(name))
+
+# or you can use print(len(input()));
+# print(len(raw_input("what is your name? "))) 
+# use raw_input instead of input alone
