de90fbd12ed2ab629174332f9395e4b39c5bc70f
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-09-20 13:09:46 +0300

    day 1

---
+++ day1.py
@@ -0,0 +1,6 @@
+# print("Hello" + " Mitch")
+
+print("Day 1 - String Manipulation")  
+print('String Conctenation is done with the "+" sign.')
+print('e.g. print("hello" + "world")')
+print("New lines can be created with a bcakslash and n.")
