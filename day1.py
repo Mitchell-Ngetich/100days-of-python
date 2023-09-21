699494a1834ba9248caf8bccf52c8ac05fbe7c50
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-09-21 12:24:43 +0300

    lists

--- day1.py
+++ day1.py
@@ -1,4 +1,5 @@
-# print("Hello" + " Mitch")
+from math import *
+#  print("Hello" + " Mitch")
 
 # print("Day 1 - String Manipulation")  
 # print('String Conctenation is done with the "+" sign.')
@@ -19,3 +20,52 @@
 # or you can use print(len(input()));
 # print(len(raw_input("what is your name? "))) 
 # use raw_input instead of input alone
+  
+# print("    /|")
+# print("   / |")
+# print("  /  |")
+# print(" /   |")
+# print("/____|")
+
+# character_name = "Tim"
+# characters_age = 40
+
+# print("There once was a man named " + character_name + ",")
+# print("he was " + str(characters_age) + " years old.") 
+# # you cannot concatenate string and integers in python
+# print("He really liked the name " + character_name)
+# print("but didn't like being " + str(characters_age) + " years old")  
+
+# phrase = "Giraffe Academy"
+# print(phrase.replace("Giraffe", "Elephant")) 
+
+# my_num = -5
+# print(sqrt(64))
+
+# name = raw_input("Enter your name: ")
+# print("Hello " + name + "!")
+
+# num1 = raw_input("Enter a number: ")
+# num2 = raw_input("Enter another number: ")
+# print(int(num2) + int(num1))
+
+# mad libs
+# color = raw_input("Enter a color: ")
+# plural_noun = raw_input("Enter a plural noun: ")
+# my_love = raw_input("Enter your love: ")
+
+# print("Roses are " + color)
+# print( plural_noun + " are blue")
+# print("I love " + my_love)
+
+# Lists = array
+
+# friends = ["Mitch", "Ruth", "Mercy", "Oscar", "Brayo"]
+# numbers = [1, 3, 5, 6, 8, 9]
+# friends.reverse()
+# print(friends)
+# like slice method in JS => takes index 1 element to index 3.
+
+# Tuples => cannot be modified
+cordinates = (4,5)
+print(cordinates[1])
